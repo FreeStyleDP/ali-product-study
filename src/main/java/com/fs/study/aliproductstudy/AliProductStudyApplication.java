bafd097eccef98ec110e73a0d2d00bfda3edeb3f
dengping <dengpingfm@163.com>
2020-06-04 11:29:29 +0800

    first

---
+++ src/main/java/com/fs/study/aliproductstudy/AliProductStudyApplication.java
@@ -0,0 +1,13 @@
+package com.fs.study.aliproductstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliProductStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliProductStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fs/study/aliproductstudy/AliProductStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.study.aliproductstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliProductStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
